b85bb75204991c03847092ed5781a3e58e88bd8c
AlexMitty <87017156+AlexMitty@users.noreply.github.com>
2021-12-06 18:03:06 +0300

    Create Code3_3.java

---
+++ 3/Code3_3.java
@@ -0,0 +1,18 @@
+class Code3_3 {
+    static boolean found;
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {{4,2,4}, {23, 112, 442}};
+
+        outer :
+        for (int [] row : matrix) {
+            for (int x : row) {
+                if (x > 100) {
+                    found = true ;
+                    break outer ;
+                }
+            }
+        }
+        System.out.println(found);
+    }
+}
