b390127696c0eeccf5de69c3edf257c7a4b9225b
AlexMitty <87017156+AlexMitty@users.noreply.github.com>
2021-12-06 14:29:48 +0300

    Create Code1_3.java

---
+++ 1/Code1_3.java
@@ -0,0 +1,14 @@
+/**
+ * Класс моей <b>первой программы</b>
+ *
+ * @version 2.1
+ * @author Кодер Типичный
+ */
+class Code1_3 {
+  public static void main(String[] args) {
+    /**
+     * Метод приветствия
+     */
+    System.out.println("Hello world!");
+  }
+}
