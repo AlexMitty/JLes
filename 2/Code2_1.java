3e217eb467fe8a4133b332c1da6f4dd4f2346bbe
AlexMitty <87017156+AlexMitty@users.noreply.github.com>
2021-12-06 16:23:39 +0300

    Create Code2_1.java

---
+++ 2/Code2_1.java
@@ -0,0 +1,8 @@
+class Code2_1 {
+  static int b;
+  public static void main(String[] args) {
+      int a;
+      System.out.println(a); // compile error: variable 'a' might not have been initialized
+      System.out.println(b); // 0
+  }
+}
