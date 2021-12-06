5301cfa29e5cdcd564f57a9475b65153fa5b6cdd
AlexMitty <87017156+AlexMitty@users.noreply.github.com>
2021-12-06 16:29:33 +0300

    Create Code2_5.java

---
+++ 2/Code2_5.java
@@ -0,0 +1,8 @@
+class Code2_5 {
+  public static void main(String[] args) {
+    String s1 = "abc";		
+    String s2 = "ab" + "c";		
+    System.out.println(s1 == s2); 	
+    System.out.println(s1.equals(s2)); 
+  }
+}
