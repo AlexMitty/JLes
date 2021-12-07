48a55efb5c6cd9653001efb78cda35e1ec230804
AlexMitty <87017156+AlexMitty@users.noreply.github.com>
2021-12-07 16:30:34 +0300

    Create ClassD.java

---
+++ 5/Diamind/ClassD.java
@@ -0,0 +1,9 @@
+package test2.Diamind;
+
+public class ClassD extends ClassC implements ClassB {
+
+    public static void main(String[] args) {
+        var d = new ClassD();
+        d.doSomething();
+    }
+}
